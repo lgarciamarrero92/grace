8f692798d977bd6db8d7a30a1fcefadb104a29a5
Luis Enrique Garcia Marrero <lgarciam@zoho.com>
2020-11-17 08:41:48 -0500

    Jetstream Start Point

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import { InertiaApp } from '@inertiajs/inertia-vue';
+import { InertiaForm } from 'laravel-jetstream';
+import PortalVue from 'portal-vue';
+/*
+import { InertiaProgress } from '@inertiajs/progress'
+InertiaProgress.init()
+*/
+Vue.mixin({ methods: { route } });
+Vue.use(InertiaApp);
+Vue.use(InertiaForm);
+Vue.use(PortalVue);
+
+const app = document.getElementById('app');
+
+
+new Vue({
+    render: (h) =>
+        h(InertiaApp, {
+            props: {
+                initialPage: JSON.parse(app.dataset.page),
+                resolveComponent: (name) => require(`./Pages/${name}`).default,
+            },
+        }),
+}).$mount(app);
